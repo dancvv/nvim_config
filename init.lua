fb7c6b2c928b8faeda0443d760020a6f31d8850b
mbp14-mountain <uquantum@hotmail.com>
2023-10-29 04:37:51 +0800

    vscode config

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 -- require lazy config
---
 require("config.keymaps")
 require("config.lazy")
 require("config.options")
+
+-- vscode envirment
+if vim.g.vscode then
+  require('config.vskeymaps')
+end
+
